8457d6113f39a108fe5765d62c3c2c9f0c226397
babayl0729 <babayl0729@gmail.com>
2024-04-22 21:07:58 -0600

    on delete cascade and right join

---
+++ one_to_many_joins/right_joins.sql
@@ -0,0 +1,9 @@
+/*
+The RIGHT JOIN keyword returns all records from the right table (orders), 
+and the matching records from the left table (customers).
+*/
+
+SELECT first_name,last_name,order_date,amount 
+FROM customers
+RIGHT JOIN orders 
+ON customers.id = orders.customer_id;
