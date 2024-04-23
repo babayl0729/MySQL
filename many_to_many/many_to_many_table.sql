dfd806fab529853e882f32c071899c0dffa16f65
babayl0729 <babayl0729@gmail.com>
2024-04-22 21:23:27 -0600

    many to many

---
+++ many_to_many/many_to_many_table.sql
@@ -0,0 +1,25 @@
+--REVIEWERS--
+CREATE TABLE reviewers(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+
+--SERIES--
+CREATE TABLE series(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+    released_year YEAR,
+    genre VARCHAR(100)
+);
+
+--REVIEWS--
+CREATE TABLE reviews(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    rating DECIMAL(2,1),
+    series_id INT,
+    reviewer_id INT,
+    FOREIGN KEY(series_id) REFERENCES series(id),
+    FOREIGN KEY(reviewer_id) REFERENCES reviewers(id)
+);
+
