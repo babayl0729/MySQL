7022f309ff4376e2ca37a5c1c6970b780af9a98f
babayl0729 <babayl0729@gmail.com>
2024-04-22 21:22:07 -0600

    right join

--- one_to_many_joins/right_joins.sql
+++ one_to_many_joins/right_joins.sql
@@ -6,4 +6,5 @@ and the matching records from the left table (customers).
 SELECT first_name,last_name,order_date,amount 
 FROM customers
 RIGHT JOIN orders 
-ON customers.id = orders.customer_id;
+ON customers.id = orders.customer_id
+GROUP BY first_name;
