b812fb70229cfc43d05b62fdb83eed528a48b957
babayl0729 <babayl0729@gmail.com>
2024-05-09 20:51:21 -0600

    ig tags

---
+++ instagram/ig_tags.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tags(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO tags(tag_name) VALUES ('sunset'), ('photography'), ('sunrise'), ('landscape'), ('food'), ('foodie'), ('delicious'), ('beauty'), ('stunning'), ('dreamy'), ('lol'), ('happy'), ('fun'), ('style'), ('hair'), ('fashion'), ('party'), ('concert'), ('drunk'), ('beach'), ('smile');
