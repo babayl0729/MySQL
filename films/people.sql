9e37b7e31b58026ee373316f15d89ddf2182ce62
babayl0729 <babayl0729@gmail.com>
2024-06-08 19:56:26 -0600

    added inserts

--- films/people.sql
+++ films/people.sql
@@ -4,3 +4,15 @@ CREATE TABLE people(
     birth_date DATE,
     deathdate DATE
 );
+
+INSERT INTO people(id,name,birth_date,deathdate)
+VALUES(1,"50 Cent","1975-07-06",""),
+    (2,"A. Michael Baldwin","1963-04-04",""),
+    (3,"A. Raven Cruz","",""),
+    (4,"A.J. Buckley","1978-02-09",""),
+    (5,"A.J. DeLucia","",""),
+    (6,"A.J. Langer","1974-05-22",""),
+    (7,"Aaliyah","1979-01-16","2001-08-25"),
+    (8,"Aaron Ashmore","1979-10-07",""),
+    (9,"Aaron Hann","",""),
+    (10,"Aaron Hill","1983-04-23","");
