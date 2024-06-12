638b41bcd29bf6539d32ec21123339348b55bc56
babayl0729 <babayl0729@gmail.com>
2024-06-11 22:27:31 -0600

    create reviews table

---
+++ films/reviews.sql
@@ -0,0 +1,9 @@
+CREATE TABLE reviews(
+    id INTEGER,
+    film_id INTEGER,
+    num_user INTEGER,
+    num_critic INTEGER,
+    imdb_score FLOAT,
+    num_votes INTEGER,
+    facebook_likes INTEGER
+);
