603c11e0512776056cf60f8f886b248befa2a4e0
babayl0729 <babayl0729@gmail.com>
2024-07-22 20:56:42 -0600

    prime ministers table and inserts

---
+++ leaders/prime_ministers.sql
@@ -0,0 +1,17 @@
+CREATE TABLE prime_ministers(
+    country VARCHAR(20),
+    continent VARCHAR(20),
+    prime_ministers VARCHAR(50)
+);
+
+INSERT INTO prime_ministers(country,continent,prime_ministers)
+VALUES("Egypt","Africa","Mostafa Madbouly"),
+("Portugal","Europe","António Costa"),
+("Pakistan","Asia","Shehbaz Sharif"),
+("United Kingdom","Europe","Boris Johnson"),
+("India","Asia","Narendra Modi"),
+("Australia","Oceania","Scott Morrison"),
+("Norway","Europe","Jonas Gahr Støre"),
+("Brunei","Asia","Hassanal Bolkiah"),
+("Oman","Asia","Haitham bin Tarik"),
+("New Zealand","Oceania","Jacinda Ardern");
